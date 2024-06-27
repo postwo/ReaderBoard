6de3023e5f651d81f548aac14675045249505307
postwo <codo7717@naver.com>
2024-06-27 19:00:35 +0900

    기본세팅 완료

---
+++ src/main/java/com/example/readerboard/ReaderBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.readerboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReaderBoardApplication.class, args);
+    }
+
+}
